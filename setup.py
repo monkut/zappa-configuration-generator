608a5cc49eb651e6e738e21ef9753a0a363c0a94
monkut <shane.cousins@gmail.com>
2018-11-30 14:42:15 +0900

    Updating to read deps from pipfile

--- setup.py
+++ setup.py
@@ -1,8 +1,33 @@
 from setuptools import setup
+import configparser
+from pathlib import Path
+import gappa
+
+
+def get_package_dependencies_from_pipfile():
+    pipfile_filepath = Path(__file__).resolve().parent / 'Pipfile'
+    assert pipfile_filepath.exists()
+    config = configparser.ConfigParser()
+    config.read(str(pipfile_filepath))
+
+    def clean_quotes(value):
+        if value.startswith('"') and value.endswith('"'):
+            value = value[1:-1]
+        return value
+
+    deps = []
+    for package_set in zip(config['packages'], config['packages'].values()):
+        required_package, required_version = package_set
+        required_package = clean_quotes(required_package)
+        required_version = clean_quotes(required_version)
+        if required_version != '*':
+            required_package = f'{required_package}{required_version}'
+        deps.append(required_package)
+    return deps
 
 setup(
     name='gappa',
-    version='0.1',
+    version=gappa.__version__,
     packages=['gappa', 'gappa.settings'],
     url='https://github.com/monkut/zappa-configuration-generator',
     license='BSD 2-Clause',
